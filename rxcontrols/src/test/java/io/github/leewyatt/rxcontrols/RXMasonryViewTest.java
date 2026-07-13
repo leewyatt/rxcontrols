e265bc64f9609aa0a70cd7292ba68f76ebc098e7
leewyatt <leewyatt7788@gmail.com>
2026-07-14 01:18:06 +0900

    test: add unit tests for RXMasonryView and RXListView to handle Double.NEGATIVE_INFINITY scroll deltas

--- rxcontrols/src/test/java/io/github/leewyatt/rxcontrols/RXMasonryViewTest.java
+++ rxcontrols/src/test/java/io/github/leewyatt/rxcontrols/RXMasonryViewTest.java
@@ -230,6 +230,7 @@ public class RXMasonryViewTest {
         RXMasonryView<String> view = new RXMasonryView<>();
         view.scrollBy(Double.NaN);
         view.scrollBy(Double.POSITIVE_INFINITY);
+        view.scrollBy(Double.NEGATIVE_INFINITY);
         assertFalse(view.hasPendingScroll(), "non-finite deltas are dropped, not armed");
         assertEquals(0.0, view.getPendingScrollDelta(), 1e-9);
         view.scrollBy(40.0);
